ab3c79953cb38a145edbf1b3a696cf5bed741611
may <may@169kang.com>
2019-09-30 16:44:11 +0800

    项目初创

---
+++ components/navBar/navBar.js
@@ -0,0 +1,57 @@
+// components/navBar/navBar.js
+Component({
+  header: {
+    type: Object,
+    value: {
+      title: "",
+      hiddenBlock: false,
+    }
+  },
+  properties: {
+    /**
+     * 自定义返回事件处理
+     * customBackReturn="{{true}}" bind:customBackReturn="customBackReturn"
+     */
+    customBackReturn: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    backClick() {
+      if (this.data.customBackReturn) {
+        this.triggerEvent("customBackReturn")
+      } else {
+        if (getCurrentPages().length == 1) {
+          wx.switchTab({
+            url: '/pages/index/index',
+          })
+        } else {
+          wx.navigateBack({
+            delta: 1
+          })
+        }
+      }
+    }
+  },
+  homeClick() {
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+  },
+  attached() {
+    var self = this;
+    wx.getSystemInfo({
+      success(res) {
+        var isIos = res.system.indexOf('iOS') > -1;
+        self.setData({
+          statusHeight: res.statusBarHeight,
+          navHeight: isIos ? 44 : 48
+        })
+      }
+    })
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,51 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '../../utils/img/1568777964097.jpg',
+      '../../utils/img/1568777964097.jpg',
+      '../../utils/img/1568777964097.jpg'
+    ],
+    indicatorDots:true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    showNav:false,
+    navData:[
+      { text: "皮表层管理",}, { text: "中胚层管理"}, { text: "痘痘肌管理"}, { text: "局部专项管理"},
+    ],
+    activeName: ['active','','','' ],
+    scrollTopNum:null
+  },
+  onPageScroll: function (e) {
+    if (e.scrollTop < 300) {
+      this.setData({ showNav: false })
+    }
+    if(e.scrollTop>=300){
+      this.setData({showNav:true})
+    }
+    if (e.scrollTop >= 300 && e.scrollTop < 1300) {
+      this.setData({ activeName: ['active', '', '', ''] })
+    }
+    if (e.scrollTop >= 1300 && e.scrollTop < 2100){
+      this.setData({ activeName: ['', 'active', '', '']})
+    }
+    if (e.scrollTop >= 2100 && e.scrollTop < 2900) {
+      this.setData({ activeName: ['', '', 'active', ''] })
+    }
+    if (e.scrollTop >= 2900) {
+      this.setData({ activeName: ['', '', '', 'active'] })
+    }
+  },
+  moveNav:function(e){
+    console.log(e)
+  },
+  handleAppointe:function(){
+    wx.navigateTo({
+      url: '../appointServe/appointServe'
+    })
+  }
+})
